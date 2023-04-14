5ad6ba6d6b7c2e75438164a9a30a2ccacaf18ed6
erizta <eriztaalifad@gmail.com>
2023-04-14 10:37:43 +0700

    Initial Animation State

---
+++ src/components/Home.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+// import motion from "framer-motion";
+import { motion } from "framer-motion/dist/framer-motion";
+
+const Home = () => {
+  return (
+    <motion.div
+      className="home container"
+      animate={{}}
+    >
+      <motion.h2 animate={{ }}>
+        Welcome to Pizza Joint
+      </motion.h2>
+      <Link to="/base">
+        <motion.button animate={{ }}>
+          Create Your Pizza
+        </motion.button>
+      </Link>
+    </motion.div>
+  );
+};
+
+export default Home;
