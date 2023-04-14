e35b784a8bc3360bf417c4c94141ca973d0bb62c
erizta <eriztaalifad@gmail.com>
2023-04-14 10:49:46 +0700

    Transition Options

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,15 +7,13 @@ const Home = () => {
   return (
     <motion.div
       className="home container"
-      animate={{}}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{delay: 1.5 , duration: 1.5}}
     >
-      <motion.h2 animate={{ }}>
-        Welcome to Pizza Joint
-      </motion.h2>
+      <motion.h2 animate={{}}>Welcome to Pizza Joint</motion.h2>
       <Link to="/base">
-        <motion.button animate={{ }}>
-          Create Your Pizza
-        </motion.button>
+        <motion.button animate={{}}>Create Your Pizza</motion.button>
       </Link>
     </motion.div>
   );
